ebb8090372efd80e2773bf8bbe863600211ec431
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2025-01-22 16:24:10 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char value;
+    printf("enter a character");
+    //scanf("%c",&value);
+    value=getchar();
+    printf("%c",value);
+    return 0;
+}    
+    
+    
+    
