fad48d8b3dd52a0668fb03e096d364e78e898089
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2025-03-17 09:50:07 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int multiply(int a,int b){
+ return a*b;
+}
+int main(){
+int num1,num2,result;
+ printf("enter two integers:");
+ scanf("%d %d",&num1,&num2);
+ result=multiply(num1,num2);
+ printf("the product of %d and %d is:%d\n",num1,num2,result);
+ return 0;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
